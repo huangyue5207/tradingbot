dfa84321eb7e5d0f0c024c8abbdf64d6724936e8
kurass <huangyue5207@gmail.com>
2022-06-27 23:15:40 +0800

    feat: add fetch last 3 years historical candles

---
+++ src/get_btcusd_historical_candles_last_3_years.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import asyncio
+import time
+
+from bfxapi import Client
+
+bfx = Client(
+  logLevel='DEBUG',
+)
+
+# now = int(round(time.time() * 1000))
+# then = now - (1000 * 60 * 60 * 24 * 10) # 10 days ago
+
+async def run():
+  end = 1656259200   # 2022-06-27
+  start = 1483200000   # 2017-01-01
+  fd = open('../data/historical_candles_last_3_years.csv', 'a+')
+  while start < end:
+    candles = await bfx.rest.get_public_candles('tBTCUSD', start=start, end=start + 24 * 60 * 60, section='hist', tf='1h', limit=100, sort=1)
+    if candles:
+      start = start + 24 * 60 * 60
+      print ("Candles:")
+      [ print (c) for c in candles ]
+      fd.write(','.join(candles))
+  fd.close()
+
+asyncio.run(run())
