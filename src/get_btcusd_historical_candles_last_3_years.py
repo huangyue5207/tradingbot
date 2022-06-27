bf6d1d429ae179d7f3a1f66a3e75029e74c8a2f6
kurass <huangyue5207@gmail.com>
2022-06-27 23:18:44 +0800

    fix: fix bugs

--- src/get_btcusd_historical_candles_last_3_years.py
+++ src/get_btcusd_historical_candles_last_3_years.py
@@ -13,11 +13,11 @@ bfx = Client(
 # then = now - (1000 * 60 * 60 * 24 * 10) # 10 days ago
 
 async def run():
-  end = 1656259200   # 2022-06-27
-  start = 1483200000   # 2017-01-01
-  fd = open('../data/historical_candles_last_3_years.csv', 'a+')
+  end = 1656259200 * 1000   # 2022-06-27
+  start = 1483200000 * 1000   # 2017-01-01
+  fd = open('./data/historical_candles_last_3_years.csv', 'a+')
   while start < end:
-    candles = await bfx.rest.get_public_candles('tBTCUSD', start=start, end=start + 24 * 60 * 60, section='hist', tf='1h', limit=100, sort=1)
+    candles = await bfx.rest.get_public_candles('tBTCUSD', start=start, end=start + 24 * 60 * 60 * 1000, section='hist', tf='1h', limit=100, sort=1)
     if candles:
       start = start + 24 * 60 * 60
       print ("Candles:")
