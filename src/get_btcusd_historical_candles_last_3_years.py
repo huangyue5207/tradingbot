3f5580fe3cb3a3829b25ba6725971d06dc021754
kurass <huangyue5207@gmail.com>
2022-06-27 23:54:35 +0800

    feat: 调整获取时间范围

--- src/get_btcusd_historical_candles_last_3_years.py
+++ src/get_btcusd_historical_candles_last_3_years.py
@@ -17,9 +17,9 @@ async def run():
   start = 1483200000 * 1000   # 2017-01-01
   fd = open('./data/historical_candles_last_3_years.csv', 'a+')
   while start < end:
-    candles = await bfx.rest.get_public_candles('tBTCUSD', start=start, end=start + 24 * 60 * 60 * 1000, section='hist', tf='1h', limit=10000, sort=1)
+    candles = await bfx.rest.get_public_candles('tBTCUSD', start=start, end=start + 24 * 60 * 60 * 30 * 1000, section='hist', tf='1h', limit=10000, sort=1)
     if candles:
-      start = start + 24 * 60 * 60 * 365
+      start = start + 24 * 60 * 60 * 30 * 1000
       print ("Candles:")
       [ print (c) for c in candles ]
       [ fd.write(','.join(map(lambda x: str(x), c)) + '\n') for c in candles ]
